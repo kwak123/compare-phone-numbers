ca767173f71f38f3a61b325a43402f22d6b7b239
kwak123 <kwak123@gmail.com>
2020-03-24 14:52:00 -0400

    add jsdoc around number

--- comparePhoneNumbers.js
+++ comparePhoneNumbers.js
@@ -1,1 +1,9 @@
+/**
+ * Compares two phone numbers in any format and returns whether they are equal
+ *
+ * If the numbers contain a country/area code, then both numbers must include area codes.
+ * @param {string} phoneNumberOne
+ * @param {string} phoneNumberTwo
+ * @returns {boolean} whether numbers are equal
+ */
 module.exports = (phoneNumberOne, phoneNumberTwo) => {};
