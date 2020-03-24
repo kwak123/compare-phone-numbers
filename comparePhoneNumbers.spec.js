36d0c1f15bf1f50703e40e5d0dc706631549a4ea
kwak123 <kwak123@gmail.com>
2020-03-24 14:40:53 -0400

    basic files

---
+++ comparePhoneNumbers.spec.js
@@ -0,0 +1,5 @@
+const comparePhoneNumbers = require('./comparePhoneNumbers');
+const assert = require('assert');
+
+const assertNumbersAreEqual = (num1, num2) =>
+  assert(num1 === num2, 'not equal!');
